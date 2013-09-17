08c0b0f95efae64544a203bb3828dea6ab120dbb
Facundo Viale <fviale@despegar.com>
2013-09-17 10:03:09 -0300

    Improve the API of the PhantomJSConsoleExecutor

--- src/main/java/com/github/jarlakxen/embedphantomjs/executor/PhantomJSConsoleExecutor.java
+++ src/main/java/com/github/jarlakxen/embedphantomjs/executor/PhantomJSConsoleExecutor.java
@@ -22,13 +22,17 @@ package com.github.jarlakxen.embedphantomjs.executor;
 import static java.util.Arrays.*;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.github.jarlakxen.embedphantomjs.PhantomJSReference;
@@ -38,15 +42,32 @@ public class PhantomJSConsoleExecutor {
 
 	private static final Logger LOGGER = Logger.getLogger(PhantomJSConsoleExecutor.class);
 
-	private static final char SYSTEM_NEWLINE[] = System.getProperty("line.separator").toString().toCharArray();
-	private static final String PHANTOMJS_CONSOLE_PREFIX = "phantomjs> ";
-	private static final List<String> PHANTOMJS_CONSOLE_POSTFIXS = asList("{}", "undefined");
+	private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+	private static final int EOF = -1;
+
+	private static final char SYSTEM_NEWLINE[] = System.getProperty("line.separator").toCharArray();
+
+	private static final String DEFAULT_PHANTOMJS_CONSOLE_PREFIX = "phantomjs> ";
+	private static final List<String> DEFAULT_PHANTOMJS_CONSOLE_POSTFIXS = asList("{}", "undefined");
+	private static final String PHANTOMJS_PARSER_ERROR_PREFIX = "Parse error";
 
 	private PhantomJSReference phantomReference;
+	private File scriptFile;
+	private String scriptArgs[];
+	private String consolePrefix;
+	private List<String> consolePostfix = Collections.emptyList();
 	private Process process;
 
 	public PhantomJSConsoleExecutor(PhantomJSReference phantomReference) {
 		this.phantomReference = phantomReference;
+		this.consolePrefix = DEFAULT_PHANTOMJS_CONSOLE_PREFIX;
+		this.consolePostfix = DEFAULT_PHANTOMJS_CONSOLE_POSTFIXS;
+	}
+
+	public PhantomJSConsoleExecutor(PhantomJSReference phantomReference, final File scriptFile, final String... scriptArgs) {
+		this.phantomReference = phantomReference;
+		this.scriptFile = scriptFile;
+		this.scriptArgs = scriptArgs;
 	}
 
 	public int getPid() {
@@ -74,8 +95,20 @@ public class PhantomJSConsoleExecutor {
 
 	public void start() {
 		try {
-			process = Runtime.getRuntime().exec(this.phantomReference.getBinaryPath());
-			process.getInputStream().read(new byte[PHANTOMJS_CONSOLE_PREFIX.length()]);
+			String cmd = this.phantomReference.getBinaryPath();
+
+			if (scriptFile != null) {
+				cmd = cmd + " " + scriptFile.getAbsolutePath();
+			}
+
+			if (scriptArgs != null && scriptArgs.length > 0) {
+				cmd = cmd + " " + StringUtils.join(scriptArgs, " ");
+			}
+
+			process = Runtime.getRuntime().exec(cmd);
+			if (StringUtils.isNotBlank(consolePrefix)) {
+				process.getInputStream().read(new byte[consolePrefix.length()]);
+			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -99,7 +132,7 @@ public class PhantomJSConsoleExecutor {
 	}
 
 	public String execute(final String scriptSource) throws UnexpectedProcessEndException {
-		return this.execute(IOUtils.toInputStream(scriptSource), PHANTOMJS_CONSOLE_POSTFIXS);
+		return this.execute(IOUtils.toInputStream(scriptSource), consolePostfix);
 	}
 
 	public String execute(final String scriptSource, String... endLines) throws UnexpectedProcessEndException {
@@ -118,11 +151,13 @@ public class PhantomJSConsoleExecutor {
 		}
 
 		try {
-			IOUtils.copy(scriptSourceInputStream, process.getOutputStream());
+			String input = copy(scriptSourceInputStream, process.getOutputStream());
 			// Append Enter to the input
 
-			for (char c : SYSTEM_NEWLINE) {
-				process.getOutputStream().write(c);
+			if (!endWithNewLine(input)) {
+				for (char c : SYSTEM_NEWLINE) {
+					process.getOutputStream().write(c);
+				}
 			}
 
 			process.getOutputStream().flush();
@@ -146,8 +181,14 @@ public class PhantomJSConsoleExecutor {
 		while (true) {
 			String line = in.readLine();
 
+			if (line.equals(PHANTOMJS_PARSER_ERROR_PREFIX)) {
+				return line;
+			}
+
 			if (line == null || endLines.contains(line)) {
-				in.skip(PHANTOMJS_CONSOLE_PREFIX.length());
+				if (StringUtils.isNotBlank(consolePrefix)) {
+					in.skip(consolePrefix.length());
+				}
 				break;
 			}
 
@@ -159,5 +200,37 @@ public class PhantomJSConsoleExecutor {
 		}
 
 		return out.toString();
+	}
+
+	private boolean endWithNewLine(String input) {
+		return input.endsWith(String.valueOf(SYSTEM_NEWLINE));
+	}
+
+	private String copy(InputStream input, OutputStream output) throws IOException {
+
+		StringBuilder inputString = new StringBuilder();
+		byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+		int n = 0;
+		while (EOF != (n = input.read(buffer))) {
+			output.write(buffer, 0, n);
+			inputString.append(new String(buffer, 0, n));
+		}
+		return inputString.toString();
+	}
+	
+	public void setConsolePrefix(String consolePrefix) {
+		this.consolePrefix = consolePrefix;
+	}
+	
+	public String getConsolePrefix() {
+		return consolePrefix;
+	}
+	
+	public void setConsolePostfix(List<String> consolePostfix) {
+		this.consolePostfix = consolePostfix;
+	}
+	
+	public List<String> getConsolePostfix() {
+		return consolePostfix;
 	}
 }
